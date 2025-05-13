9f8091e58a3b9bad0d8d61a7772249c76efd9032
yuli_1003 <yuli.aviram@mail.huji.ac.il>
2025-05-13 11:59:47 +0300

    start

--- MapReduceFramework.cpp
+++ MapReduceFramework.cpp
@@ -1,3 +1,28 @@
-//
-// Created by yulia on 12/05/2025.
-//
+#include <thread>        // std::thread
+#include <mutex>         // std::mutex, std::lock_guard
+#include <condition_variable> // std::condition_variable
+#include <chrono>        // clocks and durations
+#include <vector>
+#include <iostream>
+#include "Barrier.h"
+#include <atomic>
+
+
+class Framework{
+ private:
+  MapReduceClient client;
+  JobState state;
+  OutputVec* output;
+
+
+ public:
+  JobHandle startMapReduceJob(const MapReduceClient& client,
+                              const InputVec& inputVec, OutputVec& outputVec,
+                              int multiThreadLevel){
+    this->client = client;
+    this->output = outputVec;
+
+
+  }
+
+};
